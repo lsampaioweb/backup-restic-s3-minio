013f9cbb053728ae557a62d63712a1ad4dfee4c6
Luciano Sampaio Martins de Souza <lsampaioweb@gmail.com>
2025-10-19 10:02:04 -0300

    fix: Restore prune command in restic-backup.sh for proper backup workflow.

--- restic-backup.sh
+++ restic-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# set -e # Abort if there is an issue with any build.
+set -e # Abort if there is an issue with any build.
 
 # Usage:
 # ./restic-backup.sh local <repository>
@@ -7,4 +7,5 @@
 
 $(dirname "$0")/./backup.sh "$1" "$2"
 $(dirname "$0")/./forget.sh "$1" "$2"
+$(dirname "$0")/./prune.sh "$1" "$2"
 $(dirname "$0")/./check.sh "$1" "$2"
